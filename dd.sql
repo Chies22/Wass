a020779074da60afe02b9620d79799cd529fc0fa
Chies22 <geoffry.emil@strathmore.edu>
2024-07-01 10:20:31 +0300

    sth

--- dd.sql
+++ dd.sql
@@ -4,5 +4,9 @@ CREATE table if NOT EXISTS Freight(
 freightweight int(5) NOT null,
 freightId varchar(9) PRIMARY KEY,
 freightdestination varchar(50),
-Arrival_time date
+Arrival_time date,
+DATECREATED DATETIME NOT NULL DEFAULT current_timestamp(),
+DATEUPDATED DATETIME NOTNULL DEFAULT current_timestamp() ON UPDATE 	current_timestamp()
+
  );
+  
---
+++ index.php
@@ -0,0 +1,64 @@
+<?php include_once("templates/header.php"); ?>
+    <div class="banner">
+        <h1 style="font-size: larger;">Welcome</h1>
+    </div>
+    <div class="row">
+        <div class="content">
+        <p>To carry, collect, receive, load, unload, store, consign, distribute, transfer and deliver goods, wares, merchandise, parcels, packages, baggage, freight, animals, livestock, timber, coal, oil, ores and other minerals and other property of every description by any mode of transport.</p>
+        </div>
+        <div class="side_bar">
+            <h2>We can ship anything to the your desired destination</h2>
+            <img src="pictures/B747cargolux.jpg" alt="large freight"width="45%" height="45%">
+            <img src="pictures/mnyT.jpg" alt="A truck" width="45%" height="45%">
+        </div>
+        <div class="side_bar">
+        <ul>
+        <li>HTML</li>
+        <li>HTTP</li>
+        <li>FTP</li>
+        <li>TCP</li>
+        <li>IP</li>
+    </ul>
+    <ol type="A">
+        <li>HTML</li>
+        <li>HTTP</li>
+        <li>FTP</li>
+        <li>TCP</li>
+        <li>IP</li>
+    </ol>
+    <table border="1px" style="width:30%; border: 1px solid brown;border-collapse: collapse;">
+<tr>
+    <td>SN</td>
+    <td>DISTANCE IN NM</td>
+    <td>QUANTITY</td>
+    <td>PRICE PER NM</td>
+    <td>TOTAL PRICE</td>
+</tr>
+<tr>
+    <td>1</td>
+    <td>20</td>
+    <td>7</td>
+    <td>KSH45</td>
+    <td>KSH315</td>
+</tr>
+<tr>
+    <td>2</td>
+    <td>book</td>
+    <td>3</td>
+    <td>KSH20</td>
+    <td>KSH60</td>
+</tr>
+<tr>
+    <td>3</td>
+    <td>pen</td>
+    <td>300</td>
+    <td>KSH10</td>
+    <td>KSH3000</td>
+</tr>
+
+
+</table>
+        </div>
+    </div>
+    
+    <?php include_once("templates/footer.php");?>
